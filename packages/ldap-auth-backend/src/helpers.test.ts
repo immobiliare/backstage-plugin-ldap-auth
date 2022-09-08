a5048d5019de843ea3e89b686fe833c6277007cf
Simone Corsi <simonecorsi.dev@gmail.com>
2022-09-08 17:29:01 +0200

    --wip-- [skip ci]

--- packages/ldap-auth-backend/src/helpers.test.ts
+++ packages/ldap-auth-backend/src/helpers.test.ts
@@ -10,10 +10,30 @@ import {
     TokenValidator,
     JWTTokenValidator,
     TokenValidatorNoop,
-} from 'helpers';
+} from './helpers';
+
+import jwt from 'jsonwebtoken';
 
 describe('helpers functions', () => {
-    it('parseJwtPayload', () => {
-        expect(1).toBeDefined();
+    it('should parse jwt', () => {
+        const payload = { sub: 'username' };
+        const j = jwt.sign(payload, 'secret', {
+            expiresIn: '1min',
+        });
+
+        const decoded = parseJwtPayload(j);
+
+        expect(decoded?.sub).toBe(payload.sub);
+        expect(decoded?.iat).toBeDefined();
+        expect(decoded?.exp).toBeDefined();
+    });
+
+    it('should throw if jwt is expired', () => {
+        const payload = { sub: 'username' };
+        const j = jwt.sign(payload, 'secret', {
+            expiresIn: '-1min',
+        });
+
+        expect(() => parseJwtPayload(j)).toThrow();
     });
 });
