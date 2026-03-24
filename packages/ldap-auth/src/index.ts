2024b523791921ea98e00c56009e8b944554859a
Simone Corsi <simonecorsi.dev@gmail.com>
2026-03-24 13:37:00 +0100

    refactor: reformat plugin exports for improved readability

--- packages/ldap-auth/src/index.ts
+++ packages/ldap-auth/src/index.ts
@@ -1,4 +1,8 @@
-export { ldapAuthFrontendPlugin as ldapAuthPlugin, LdapAuthFrontendPage } from './plugin';
+export {
+    ldapAuthFrontendPlugin,
+    ldapAuthPlugin,
+    LdapAuthFrontendPage,
+} from './plugin';
 export { LdapSignInPage } from './components/LoginPage/LoginPage';
 export type { LdapSignInPageProps } from './components/LoginPage/LoginPage';
 export type { LoginFormStyles } from './components/LoginPage/Form';
